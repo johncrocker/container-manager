dc068fe2fc687eb23c99f72f2002c6bff723ce13
John Crocker <code.chef609@silomails.com>
2024-03-01 15:09:48 +0000

    Layout

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var  hbs = require('hbs');
+var handlebars = require('express-handlebars');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -10,8 +10,11 @@ var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
+
+app.engine('hbs', handlebars.engine({extname: '.hbs', defaultLayout: 'layout'}));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
