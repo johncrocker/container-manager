3909855ae9508f23ab20822de7ea07d2f276fb0f
John Crocker <code.chef609@silomails.com>
2024-03-01 14:28:44 +0000

    Added views

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var  hbs = require('hbs');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -10,6 +10,8 @@ var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
