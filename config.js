220ade492b0ef9835f7417d477da1daf8824420a
John Crocker <code.chef609@silomails.com>
2024-03-10 00:18:01 +0000

    nconf config

---
+++ config.js
@@ -0,0 +1,39 @@
+var path = require('path');
+ var nconf = require('nconf');
+
+  //
+  //  any overrides
+  //
+  nconf.overrides({
+    'always': 'be this value'
+  });
+
+  //
+  //  `process.env`
+  //  `process.argv`
+  //
+  nconf.env().argv();
+
+  //
+  // Values in `config.json`
+  //
+  nconf.file(path.join(__dirname, 'config', 'config.json'));
+
+  //
+  // Or searching from a base directory.
+  // Note: `name` is optional.
+  //
+  nconf.file(name, {
+    file: 'config.json',
+    dir: path.join(__dirname, 'config')),
+    search: true
+  });
+
+  //
+  // Any default values
+  //
+  nconf.defaults({
+    'if nothing else': 'use this value'
+  });
+
+  module.exports = nconf;
